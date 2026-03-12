ad6c2959cf731783561880191a5e05052b3a75ad
Sean Carroll <seanwilliamcarroll@gmail.com>
2026-03-12 11:59:01 -0400

    Super rough, inefficient stab at UF

--- compiler_systems/src/union_find.hpp
+++ compiler_systems/src/union_find.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <iostream>
+#include <memory>
+#include <unordered_set>
+
 // Union-Find (Disjoint Set Union)
 //
 // TODO: Implement with path compression + union by rank.
@@ -7,18 +11,52 @@
 class UnionFind {
 public:
   explicit UnionFind(int n) {
-    (void)n; // TODO
+    for (int node = 0; node < n; ++node) {
+      auto iter = node_to_sets.emplace(
+          node, std::make_shared<std::unordered_set<int>>());
+      auto &node_set = *(iter.first->second);
+      node_set.insert(node);
+    }
   }
 
   int find(int x) {
-    (void)x; // TODO
-    return -1;
+    const auto &x_set = node_to_sets[x];
+    return *std::min_element(x_set->begin(), x_set->end());
   }
 
   void unite(int x, int y) {
-    (void)x; // TODO
-    (void)y;
+    if (connected(x, y)) {
+      return;
+    }
+
+    // Find the sets, pop from one into the other, update nodes to point to this
+    // set as they are transfered
+    const auto &x_set = node_to_sets[x];
+    const auto y_set = node_to_sets[y];
+
+    for (auto node : *y_set) {
+      x_set->insert(node);
+      node_to_sets[node] = x_set;
+    }
   }
 
   bool connected(int x, int y) { return find(x) == find(y); }
+
+  void print() const {
+    std::cout << "Print:" << std::endl;
+    for (const auto &[node, node_set] : node_to_sets) {
+      std::cout << node << " : ";
+      if (!node_set) {
+        std::cout << "NULL" << std::endl;
+        continue;
+      }
+      for (auto element : *node_set) {
+        std::cout << element << ", ";
+      }
+      std::cout << std::endl;
+    }
+  }
+
+  std::unordered_map<int, std::shared_ptr<std::unordered_set<int>>>
+      node_to_sets;
 };
--- compiler_systems/src/union_find_test.cpp
+++ compiler_systems/src/union_find_test.cpp
@@ -14,3 +14,14 @@ TEST(UnionFindTest, BasicUnion) {
   EXPECT_TRUE(uf.connected(0, 1));
   EXPECT_FALSE(uf.connected(0, 2));
 }
+
+TEST(UnionFindTest, Example1) {
+  UnionFind uf(5);
+  uf.unite(0, 1);
+  uf.unite(1, 2);
+  uf.unite(3, 4);
+  EXPECT_TRUE(uf.connected(0, 1));
+  EXPECT_TRUE(uf.connected(0, 2));
+  EXPECT_FALSE(uf.connected(0, 3));
+  EXPECT_FALSE(uf.connected(4, 2));
+}
